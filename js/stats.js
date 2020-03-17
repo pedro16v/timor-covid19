b6c8242ac924eb304f930935fff130361e7fbc28
Pedro Almeida <pedro16v@gmail.com>
2020-03-17 12:56:16 +0900

    Initial commit

---
+++ js/stats.js
@@ -0,0 +1,36 @@
+(function(){
+    'use strict';
+
+    const apiURL = "https://thevirustracker.com/free-api?countryTotal=TL";
+    
+    $.ajax({
+      url: apiURL,
+      dataType: 'json',
+      success: function(data) {
+        var countryData = data.countrydata[0];
+        var countryNews = data.countrynewsitems[0];
+
+        loadStats(countryData);
+        loadNews(countryNews);
+      }
+    });
+    
+
+    function loadStats(countryData) {
+        $("#total_cases").text(countryData.total_cases);
+        $("#total_active_cases").text(countryData.total_active_cases);
+        $("#total_serious_cases").text(countryData.total_serious_cases);
+        $("#total_deaths").text(countryData.total_deaths);
+        $("#total_recovered").text(countryData.total_recovered);
+        $("#total_unresolved").text(countryData.total_unresolved);
+        $("#total_new_cases_today").text(countryData.total_new_cases_today);
+        $("#total_new_deaths_today").text(countryData.total_new_deaths_today);
+    }
+
+    function loadNews(countryNews) {
+        // console.log(countryNews);
+        $("#newslist").html(Handlebars.templates.news_list(countryNews));
+        $("#newslist").html($(".notification").get().reverse());
+    }
+
+}())
---
+++ js/templates.js
@@ -0,0 +1,39 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['news_list'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return ((stack1 = lookupProperty(helpers,"if").call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? lookupProperty(depth0,"title") : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":3,"column":8},"end":{"line":11,"column":15}}})) != null ? stack1 : "");
+},"2":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "            <a href=\""
+    + alias4(((helper = (helper = lookupProperty(helpers,"url") || (depth0 != null ? lookupProperty(depth0,"url") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data,"loc":{"start":{"line":4,"column":21},"end":{"line":4,"column":28}}}) : helper)))
+    + "\" target=\"_blank\" class=\"notification\">\n                <div class=\"notification-icon\">\n                    <i class=\"fas fa-inbox\"></i>\n                </div>\n                <div class=\"notification-text\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":8,"column":47},"end":{"line":8,"column":56}}}) : helper)))
+    + "</div>\n                <span class=\"notification-time\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"time") || (depth0 != null ? lookupProperty(depth0,"time") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"time","hash":{},"data":data,"loc":{"start":{"line":9,"column":48},"end":{"line":9,"column":56}}}) : helper)))
+    + "</span>\n            </a>\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"notifications\" >\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":4},"end":{"line":12,"column":13}}})) != null ? stack1 : "")
+    + "\n    <div class=\"notifications-show-all\">\n        <a href=\"#!\">Show all</a>\n    </div>\n</div>";
+},"useData":true});
+})();
