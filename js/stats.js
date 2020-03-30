4c2e857c97792a76872d5b8cce6c4d2892f6e8f9
Pedro Almeida <pedro16v@gmail.com>
2020-03-30 20:13:09 +0900

    Added page 2

--- js/stats.js
+++ js/stats.js
@@ -3,9 +3,49 @@
 
     const defaultCoutry = "TL"
     const hostname = document.location.hostname;
-    const apiURL = "https://thevirustracker.com/free-api?countryTotal=";
+
+    var countriesByCode;
+    (function() {
+        $.ajax({
+          url: '/node_modules/country-json/src/country-by-abbreviation.json',
+          dataType: 'json',
+          success: function(data) {
+            countriesByCode = data;
+          }
+        });
+    }());
+    
+    var countriesByPopulation;
+    (function() {
+        $.ajax({
+          url: '/node_modules/country-json/src/country-by-population.json',
+          dataType: 'json',
+          success: function(data) {
+            countriesByPopulation = data;
+          }
+        });
+    }());
+
+    var countriesByTemperature;
+    (function() {
+        $.ajax({
+          url: '/node_modules/country-json/src/country-by-yearly-average-temperature.json',
+          dataType: 'json',
+          success: function(data) {
+            countriesByTemperature = data;
+          }
+        });
+    }());
+
+    const apiURL = "https://thevirustracker.com/free-api?";
+    const countryTotalURL = apiURL + "countryTotal=";
+    const globalStatsURL = apiURL + "global=stats";
+    const allCountryTotalsURL = apiURL + "countryTotals=ALL";
+
     const tatoliURL = "/json/tatoli.json";
     const countriesURL = "/json/countries.json";
+
+    var allCountryTotalsData;
 
 
     function loadStats(countryData) {
@@ -20,6 +60,7 @@
     }
 
     function loadingStats(loadingText="loading...") {
+
         $("#total_cases").text(loadingText);
         $("#total_active_cases").text(loadingText);
         $("#total_serious_cases").text(loadingText);
@@ -57,25 +98,204 @@
         loadingStats();
         
         $.ajax({
-          url: apiURL + country,
+          url: countryTotalURL + country,
           dataType: 'json',
           success: function(data) {
-            var countryData = data.countrydata[0];
-            var countryNews = data.countrynewsitems[0];
 
-            loadStats(countryData);
-            loadNews(countryNews);
+            loadStats(data.countrydata[0]);
 
+            // loadNews(data.countrynewsitems[0]);
             // loadTatoli();
           }
         });
     }
+
+    function loadSortedByCases(data) {
+        data.sort((a, b) => (a.total_cases < b.total_cases) ? 1 : -1);
+        $("#content").html(Handlebars.templates.table(data));
+        loadSortingOptions();
+    }
+
+    function loadSortingOptions() {
+        var data = allCountryTotalsData;
+
+        $(".col-cases").on("click", function(){
+            loadSortedByCases(data);            
+        });
+        $(".col-deaths").on("click", function(){
+            data.sort((a, b) => (a.total_deaths < b.total_deaths) ? 1 : -1);
+            $("#content").html(Handlebars.templates.table(data));
+            loadSortingOptions();
+        });
+        $(".col-temperature").on("click", function(){
+            data.sort((a, b) => (a.country_temperature < b.country_temperature) ? 1 : -1);
+            $("#content").html(Handlebars.templates.table(data));
+            loadSortingOptions();
+        });
+        $(".col-ratio1").on("click", function(){
+            data.sort((a, b) => (a.death_ratio_per_infected < b.death_ratio_per_infected) ? 1 : -1);
+            $("#content").html(Handlebars.templates.table(data));
+            loadSortingOptions();
+        });
+        $(".col-population").on("click", function(){
+            data.sort((a, b) => (a.country_population < b.country_population) ? 1 : -1);
+            $("#content").html(Handlebars.templates.table(data));
+            loadSortingOptions();
+        });
+        $(".col-ratio2").on("click", function(){
+            data.sort((a, b) => (a.death_ratio_per_population < b.death_ratio_per_population) ? 1 : -1);
+            $("#content").html(Handlebars.templates.table(data));
+            loadSortingOptions();
+        });
+        $(".col-ratio3").on("click", function(){
+            data.sort((a, b) => (a.death_ratio_per_temperature < b.death_ratio_per_temperature) ? 1 : -1);
+            $("#content").html(Handlebars.templates.table(data));
+            loadSortingOptions();
+        });
+    }
+
+    function loadPage2(){
+
+        $.ajax({
+          url: allCountryTotalsURL,
+          dataType: 'json',
+          success: function(data) {
+
+            var data = data.countryitems[0];
+            var list = [];
+            
+            for (var key of Object.keys(data)) {
+                var country_title = data[key].title;
+
+                // if (country_title in ["Democratic Republic of Congo", "DR Congo"]) {
+                //     continue;
+                // }
+
+                var total_cases = data[key].total_cases;
+                var total_deaths = data[key].total_deaths;
+
+                // --- ADD death_ratio_per_infected
+                var death_ratio_per_infected;
+                
+                if (total_deaths > 0 && total_cases > 0) {
+                    death_ratio_per_infected = parseFloat((total_deaths / total_cases) * 100).toFixed(2);
+                } else {
+                    death_ratio_per_infected = 0;
+                }
+                
+                try {
+                    data[key]["death_ratio_per_infected"] = Number(death_ratio_per_infected);
+                } catch(err) {
+                    console.log(err);
+                }
+
+                // --- ADD POPULATION
+                var country_code = data[key].code;
+                var country_name = function(){
+                    for (var i = 0; i < countriesByCode.length; i++) {
+                        if (country_code == countriesByCode[i].abbreviation) {
+                            return countriesByCode[i].country;
+                        }
+                    }
+                }();
+                
+                var country_population = function(){
+                    for (var i = 0; i < countriesByPopulation.length; i++) {
+                        if (country_name == countriesByPopulation[i].country) {
+                            return parseInt(countriesByPopulation[i].population);
+                        }
+                    }
+                }(); 
+
+                try {
+                    data[key]["country_population"] = country_population;
+                } catch(err) {
+                    console.log(err);
+                }
+
+                // --- ADD COUNTRY TEMPERATURE RATIO               
+
+                var country_temperature = function(){
+                    for (var i = 0; i < countriesByTemperature.length; i++) {
+                        if (country_name == countriesByTemperature[i].country) {
+                            return parseInt(countriesByTemperature[i].temperature);
+                        }
+                    }
+                }();
+
+                try {
+                    data[key]["country_temperature"] = country_temperature;
+                } catch(err) {
+                    console.log(err);
+                }
+
+                // --- ADD POPULATION RATIO
+
+                var death_ratio_per_population;
+
+                if (total_deaths > 0 && country_population > 0) {
+                    death_ratio_per_population = parseFloat((total_deaths / country_population) * 1000000).toFixed(2);
+                } else {
+                    death_ratio_per_population = 0;
+                }
+
+                try {
+                    data[key]["death_ratio_per_population"] = Number(death_ratio_per_population);
+                    
+                } catch(err) {
+                    console.log(err);
+                    console.log(data[key]);
+                }
+
+                // --- ADD TEMPERATURE RATIO
+
+                var death_ratio_per_temperature;
+
+                if (total_deaths > 0 && country_population > 0) {
+                    death_ratio_per_temperature = parseInt((total_deaths / country_temperature));
+                } else {
+                    death_ratio_per_temperature = 0;
+                }
+
+                try {
+                    data[key]["death_ratio_per_temperature"] = Number(death_ratio_per_temperature);
+                    list.push(data[key]);
+                } catch(err) {
+                    console.log(err);
+                    console.log(data[key]);
+                }
+
+                
+            }
+
+            allCountryTotalsData = list;
+            loadSortedByCases(allCountryTotalsData);
+
+            loadSortingOptions();
+            
+          }
+        });
+
+    }
+
+    // ----------------------------------------
 
     function enableDropDown() {
         $("#country_list").on("change", function(){
             loadData($(this).val());
         })
     }
+
+    function enablePagination() {
+        $(".page1").on("click", function(){
+            location.reload();
+        });
+        $(".page2").on("click", function(){
+            loadPage2();
+        });
+    }
+
+    // ----------------------------------------
 
     $.ajax({
         url: countriesURL,
@@ -84,8 +304,12 @@
             $("#country_list").html(Handlebars.templates.country_list(data));
             $("#country_list").val(defaultCoutry);
 
+            enablePagination();
             enableDropDown();
             loadData(defaultCoutry);
+            // loadPage2();
+
+
         }
     });    
 
--- js/templates.js
+++ js/templates.js
@@ -59,4 +59,41 @@ templates['news_list'] = template({"1":function(container,depth0,helpers,partial
     + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":2,"column":4},"end":{"line":12,"column":13}}})) != null ? stack1 : "")
     + "\n    <div class=\"notifications-show-all\">\n        <a href=\"#!\">Show all</a>\n    </div>\n</div>";
 },"useData":true});
+templates['table'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=container.hooks.helperMissing, alias3="function", alias4=container.escapeExpression, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "                            <tr>                                \n                                <th scope=\"row\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"title") || (depth0 != null ? lookupProperty(depth0,"title") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"loc":{"start":{"line":28,"column":48},"end":{"line":28,"column":57}}}) : helper)))
+    + "</th>   \n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"country_population") || (depth0 != null ? lookupProperty(depth0,"country_population") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"country_population","hash":{},"data":data,"loc":{"start":{"line":29,"column":55},"end":{"line":29,"column":77}}}) : helper)))
+    + "</td>\n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"total_cases") || (depth0 != null ? lookupProperty(depth0,"total_cases") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"total_cases","hash":{},"data":data,"loc":{"start":{"line":30,"column":55},"end":{"line":30,"column":70}}}) : helper)))
+    + "</td>\n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"total_deaths") || (depth0 != null ? lookupProperty(depth0,"total_deaths") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"total_deaths","hash":{},"data":data,"loc":{"start":{"line":31,"column":55},"end":{"line":31,"column":71}}}) : helper)))
+    + "</td>\n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"country_temperature") || (depth0 != null ? lookupProperty(depth0,"country_temperature") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"country_temperature","hash":{},"data":data,"loc":{"start":{"line":32,"column":55},"end":{"line":32,"column":78}}}) : helper)))
+    + "</td>\n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"death_ratio_per_infected") || (depth0 != null ? lookupProperty(depth0,"death_ratio_per_infected") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"death_ratio_per_infected","hash":{},"data":data,"loc":{"start":{"line":33,"column":55},"end":{"line":33,"column":83}}}) : helper)))
+    + " %</td>\n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"death_ratio_per_population") || (depth0 != null ? lookupProperty(depth0,"death_ratio_per_population") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"death_ratio_per_population","hash":{},"data":data,"loc":{"start":{"line":34,"column":55},"end":{"line":34,"column":85}}}) : helper)))
+    + "</td>\n                                <td class=\"text-right\">"
+    + alias4(((helper = (helper = lookupProperty(helpers,"death_ratio_per_temperature") || (depth0 != null ? lookupProperty(depth0,"death_ratio_per_temperature") : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"death_ratio_per_temperature","hash":{},"data":data,"loc":{"start":{"line":35,"column":55},"end":{"line":35,"column":86}}}) : helper)))
+    + "</td>\n                            </tr>\n";
+},"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<div class=\"row dash-row\">\n    <div class=\"col-xl-12\">\n\n        <div class=\"card spur-card\">\n            <div class=\"card-header\">\n                <div class=\"spur-card-icon\">\n                    <i class=\"fas fa-table\"></i>\n                </div>\n                <div class=\"spur-card-title\">Default table</div>\n            </div>\n            <div class=\"card-body \">\n                <table class=\"table table-in-card\">\n                    <thead>\n                        <tr>\n                            <th scope=\"col\">country</th>\n                            <th scope=\"col\"><a class=\"col-population\" href=\"#\">population</a></th>\n                            <th scope=\"col\"><a class=\"col-cases\" href=\"#\">cases</a></th>\n                            <th scope=\"col\"><a class=\"col-deaths\" href=\"#\">deaths</a></th>\n                            <th scope=\"col\"><a class=\"col-temperature\" href=\"#\">yearly avg temp</a></th>\n                            <th scope=\"col\"><a class=\"col-ratio1\" href=\"#\">deaths/case</a></th>                            \n                            <th scope=\"col\"><a class=\"col-ratio2\" href=\"#\">deaths/population</br> per million</a></th>\n                            <th scope=\"col\"><a class=\"col-ratio3\" href=\"#\">deaths/temperature</a></th>\n                        </tr>\n                    </thead>\n                    <tbody>\n"
+    + ((stack1 = lookupProperty(helpers,"each").call(depth0 != null ? depth0 : (container.nullContext || {}),depth0,{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data,"loc":{"start":{"line":26,"column":24},"end":{"line":37,"column":33}}})) != null ? stack1 : "")
+    + "                    </tbody>\n                </table>\n            </div>\n        </div>\n\n    </div>\n</div>";
+},"useData":true});
 })();
